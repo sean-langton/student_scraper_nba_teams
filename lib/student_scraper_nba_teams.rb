b874a630257b19fbd09ad57f891366584b1b4271
Sean Langton <slangton1@gmail.com>
2018-12-17 18:10:33 -0500

    First Commit
    Bundler Setup

---
+++ lib/student_scraper_nba_teams.rb
@@ -0,0 +1,6 @@
+require "student_scraper_nba_teams/version"
+
+module StudentScraperNbaTeams
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/student_scraper_nba_teams/version.rb
@@ -0,0 +1,3 @@
+module StudentScraperNbaTeams
+  VERSION = "0.1.0"
+end
