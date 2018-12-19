e8e4a85aca73d0f8bbe02d3281fd60781a93c476
Sean Langton <slangton1@gmail.com>
2018-12-18 22:47:31 -0500

    Added team link to scrape_teams

--- lib/team_scraper.rb
+++ lib/team_scraper.rb
@@ -11,6 +11,7 @@ class Scraper
     doc = Nokogiri::HTML(open("https://www.basketball-reference.com/teams/"))
     doc.css(".full_table").each do |team|
       name = team.css("th").text
+      team_link = team.css("a").attribute("href").value
       year_started = team.css("td")[1].text
       wins = team.css("td")[5].text
       losses = team.css("td")[6].text
@@ -18,11 +19,16 @@ class Scraper
       div_champ = team.css("td")[9].text
       conf_champ = team.css("td")[10].text
       league_champ = team.css("td")[11].text
-      team = {name:name, founded:year_started, wins:wins, losses:losses, playoff_app:playoffs, div_champ:div_champ, conf_champ:conf_champ, league_champ:league_champ}
+      team = {name:name, link:team_link, founded:year_started, wins:wins, losses:losses, playoff_app:playoffs, div_champ:div_champ, conf_champ:conf_champ, league_champ:league_champ}
       scraped_teams.push(team)
     end
     scraped_teams
   end
+
+  def self.scrape_players
+
+  end
+
 end
 
 binding.pry
