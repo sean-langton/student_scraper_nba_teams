6d38a3ee0f4f5a25506545e11973258b750c7774
Sean Langton <slangton1@gmail.com>
2018-12-27 23:57:31 -0500

    List Teams and Historical Summary Complete

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,12 +3,13 @@ require_relative "team.rb"
 require_relative "season.rb"
 
 class CLI
+
   BASE_PATH = "https://www.basketball-reference.com/teams/"
 
   def run
     make_teams
     add_seasons
-    display_teams
+    menu
   end
 
   def make_teams
@@ -24,7 +25,30 @@ class CLI
     end
   end
 
-  def display_teams
+  def menu
+    puts "Welcome to the Main Menu. What Can I Do For You?".colorize(:color => :black, :background => :white)
+    puts "1. List All Teams"
+    puts "2. Historical Summary"
+    puts "3. Search By Team Name"
+    puts "4. Search By Season"
+    puts "Or type " + "EXIT ".colorize(:red) + "to Exit."
+    input = gets.strip
+      if input == "1" then list_teams
+      elsif input == "2" then historical_summary
+      elsif input == "3" then search_team
+      elsif input == "4" then search_season
+      elsif input == "EXIT" then exit!
+      end
+    menu
+  end
+
+  def list_teams
+    Team.all.each do |team|
+      puts "#{team.name}".colorize(:light_green)
+    end
+  end
+
+  def historical_summary
     Team.all.each do |team|
       best_performance =
         if team.league_champ.to_i > 0 then "League Champions (#{team.league_champ} times)"
@@ -32,12 +56,12 @@ class CLI
         elsif team.div_champ.to_i > 0 then "Divison Champions (#{team.div_champ} times)"
         else "making (#{team.playoff_app} playoff appearences)"
         end
-      puts "#{team.name}".colorize(:light_blue)
+      puts "#{team.name}".colorize(:cyan)
       puts "Founded In: " + "#{team.founded}".colorize(:green)
       puts "Best Performance: " + "#{best_performance}".colorize(:green)
       puts "Historical Performance: " + "#{team.historic_wins} - #{team.historic_losses} (#{team.historic_win_pct})".colorize(:green)
-#      puts "Current Record: " + "#{team.historic_wins} - #{team.historic_losses} (#{team.historic_win_pct})".colorize(:green)
+      puts "-----------------------".colorize(:blue)
+    end
   end
 
-end
 end
