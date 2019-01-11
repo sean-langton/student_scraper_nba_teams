32afc40f6c49c15a8b2cebc007cd719cedbd860a
Sean Langton <slangton1@gmail.com>
2019-01-11 10:03:11 -0500

    Fixed grammer issue with CLI error message

--- lib/cli.rb
+++ lib/cli.rb
@@ -58,7 +58,7 @@ class CLI
   def search_team
     puts "Please Input Full Team Name" + " E.G. New York Knicks".colorize(:light_blue)
     input = gets.strip
-      if Team.find(input) == nil then puts "Sorry, That Was Not A Value Team Team"
+      if Team.find(input) == nil then puts "Sorry, That Was Not A Valid Value. Please Try Again."
       else Team.find(input).summary
            Team.find(input).seasons.each {|season|
              puts "#{season.year} "+ "#{season.wins} - #{season.losses}  #{season.playoffs}".colorize(:green)
@@ -69,7 +69,7 @@ class CLI
     def search_season
       puts "Please Input 7 Character Season" + " E.G. 2001-02".colorize(:light_blue)
       input = gets.strip
-      if Season.find(input) == nil then puts "Sorry, That Was Not A Value Team Team"
+      if Season.find(input) == nil then puts "Sorry, That Was Not A Valid Input. Please Try Again."
       else sel_seasons_array = Season.find(input)
         sel_seasons_array.each {|season|
         puts "#{season.team_name}   #{season.wins}-#{season.losses}   #{season.playoffs}"
